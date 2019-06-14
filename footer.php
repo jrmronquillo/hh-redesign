0f3db3e3f3b283c10fdff45aa2580e0bc78bfa73
jrmronquillo <jrm.ronquillo@gmail.com>
2019-06-14 06:48:58 -0700

    add theme thumbnail and name it screenshot.png

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer class="footer-container">
+
+            <p>
+                Jerome Ronquillo
+                <i class="fas fa-cookie"></i>
+                <a href="http://twitter.com"><i class="fab fa-twitter-square"></i></a>
+                <a href="http://google.com"><i class="fab fa-instagram"></i></a>
+            </p>
+
+
+            <?php wp_footer(); ?>
+        </footer>
+    </body>
+</html>
