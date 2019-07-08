2894385cd22974e15715b1095ff42681d6fa4e9e
jrmronquillo <jrm.ronquillo@gmail.com>
2019-07-08 06:29:16 -0700

    test commit

--- header.php
+++ header.php
@@ -18,10 +18,10 @@
 		      <div class="collapse navbar-collapse" id="navbarSupportedContent">
 		        <ul class="navbar-nav mr-auto">
 		          <li class="nav-item active">
-		            <a class="nav-link" href="#">Portfolio <span class="sr-only">(current)</span></a>
+		            <a class="nav-link" href="/portfolio">Portfolio <span class="sr-only">(current)</span></a>
 		          </li>
 		          <li class="nav-item active">
-		            <a class="nav-link disabled" href="#">Blog</a>
+		            <a class="nav-link" href="/blog">Blog</a>
 		          </li>
                   <li>
                     <a class="nav-link" href="/contact">Contact</a>
